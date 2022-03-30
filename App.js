33e9d855a79ce5d8132648591e0a62094089bd33
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2022-03-30 00:02:33 -0300

    import fonts and apploading-expo

--- App.js
+++ App.js
@@ -3,8 +3,23 @@ import { StyleSheet, View } from 'react-native';
 import react from 'react';
 import SplashScreen from './src/SplashScreen/login'
 import { CustomText } from './src/components/atoms/CustomText';
+import AppLoading from 'expo-app-loading';
+import { 
+    useFonts, 
+    SourceSansPro_400Regular,
+    SourceSansPro_700Bold,
+    SourceSansPro_600SemiBold,
+    SourceSansPro_900Black,
+   } from '@expo-google-fonts/source-sans-pro';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    SourceSansPro_400Regular,
+    SourceSansPro_700Bold,
+    SourceSansPro_600SemiBold,
+    SourceSansPro_900Black,
+  });
+
   return (
     <SplashScreen />
     
